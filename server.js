5875f88180d8bb0666a930157839a7ffbb744fb4
ginolanetti <maleckitoma@gmail.com>
2021-02-17 11:35:57 +0100

    static site served

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3010;
 
+
+app.use(express.static('public'))
 //GET, PUT, POST, DELETE endpoints
 
 //(location, middleware[optional], callback)
-app.get('/', (req, res) =>{
-  res.send('<h1> Hi there, folks345! </h1>')
-}) 
+
+
+
 
 app.listen(port, () => console.log(`I'm listening on port ${port}`))
