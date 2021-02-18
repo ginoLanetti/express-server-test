92dc19a7062c821d9f3ca4f0538d3ae3658761c9
ginolanetti <maleckitoma@gmail.com>
2021-02-18 16:07:27 +0100

    one more time

--- server.js
+++ server.js
@@ -7,12 +7,6 @@ global.rootRequire = function(name) {
   return require(__dirname + '/' + name);
 };
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-
-db.on('error', (error) => console.error(error))
-db.once('open', (error) => console.log('connected to db'))
-
 
 const app = express();
 const port = process.env.PORT || 3010;
